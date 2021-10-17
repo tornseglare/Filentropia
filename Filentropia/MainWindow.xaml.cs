8431d467fc8acfb07aa8b2fa73e258a2e39305b8
Jon Lennryd <jonlennryd@yahoo.se>
2021-10-17 21:26:58 +0200

    folder listener now in its own class, FileEvents. Backed by the record FileEvent. This allows for several folder listeners.

--- Filentropia/MainWindow.xaml.cs
+++ Filentropia/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 
 using Microsoft.WindowsAPICodePack.Dialogs;
 using System.IO;
+using System.Diagnostics;
 
 namespace Filentropia
 {
@@ -25,18 +26,212 @@ namespace Filentropia
         FolderShared
     }
 
-    public class FileEvent
+    public enum eFileEvents
     {
+        Deleted,
+        Renamed,
+        Created,
+        Changed,
+    }
 
+    /// <summary>
+    /// Example of new 'init':
+    ///     var fe = new FileEvent() { FileName = "coolfilename.wow" };
+    /// 
+    /// Now fe.FileName is readonly since it only has a getter.
+    /// 
+    /// You can create a copy of a record and change its values in this way:
+    ///     var changed_fe = fe with { FileName = "muchcoolerfilename.yeah" };
+    /// 
+    /// https://www.infoworld.com/article/3607372/how-to-work-with-record-types-in-csharp-9.html
+    /// 
+    /// Why 'record' ? 
+    ///     Equals() are comparing every member by default, so I dont have to write my own.
+    ///     More here: https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/proposals/csharp-9.0/records
+    /// </summary>
+    public record FileEvent
+    {
+        // FileName is the identier for the file.
+        // The init means we can set this 'readonly' value when creating a new FileEvent. After creation this value is readonly since it only has a getter.
+        // 'private set' makes it settable in this class functions.
+        // 'protected set' makes it settable in deriving classes as well.
+        public string FileName { get; init; }
+        
+        public eFileEvents Event { get; init; }
+    }
+
+    public record FileNameChangedEvent : FileEvent
+    {
+        public string OldFileName { get; init; }
     }
 
     public class FileEvents
     {
         private List<FileEvent> fileEvents = new List<FileEvent>();
 
-        public void Add(FileEvent fileEvent)
+        public void FileDeleted(string fileName)
         {
-            fileEvents.Add(fileEvent);
+            FileEvent fe = new FileEvent() { Event = eFileEvents.Deleted, FileName = fileName };
+            fileEvents.Add(fe);
+            Debug.Write(fileEvents);
+         }
+        
+        public void FileCreated(string fileName)
+        {
+            FileEvent fe = new FileEvent() { Event = eFileEvents.Created, FileName = fileName };
+            fileEvents.Add(fe);
+            Debug.Write(fileEvents);
+        }
+
+        public void FileChanged(string fileName)
+        {
+            FileEvent fe = new FileEvent() { Event = eFileEvents.Changed, FileName = fileName };
+
+            // Since a file change event happens twice, we just ignore the second event.
+            // Please note the danger in this: if a file's content changes (very) frequently, this code will 
+            // ignore any later changes as long as the file has not been uploaded. 
+            //   <-This is on the other hand not dangerous as long as there is no danger of missing a file change
+            //     in the moment when the file gets uploaded.
+            //    TODO: To avoid this, pop the element from the list FIRST, and after that, upload the file. 
+            //          The effect of doing this is that we do not miss any file change events.
+            // 
+            if (fileEvents.FindIndex(x => x.FileName == fileName && x.Event == eFileEvents.Changed) == -1)
+            {
+                fileEvents.Add(fe);
+                Debug.Write(fileEvents);
+            }
+        }
+
+        public void FileRenamed(string oldFileName, string newFileName)
+        {
+            FileNameChangedEvent fe = new FileNameChangedEvent() {
+                Event = eFileEvents.Renamed, FileName = newFileName, OldFileName = oldFileName };
+
+            fileEvents.Add(fe);
+            Debug.Write(fileEvents);
+        }
+    }
+
+    /// <summary>
+    /// Keeps track of a single folder and its contents.
+    /// </summary>
+    public class FolderListener : IDisposable
+    {
+        private string folderPath;
+        private FileSystemWatcher watcher;
+
+        private FileEvents fileEvents = new FileEvents();
+
+        public FolderListener(string folderPath)
+        {
+            this.folderPath = folderPath;
+
+            try
+            {
+                SetupFolderListener();
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Could not setup folder listeners for " + folderPath, ex);
+            }
+        }
+
+        private void RemoveFolderListener()
+        {
+            if (watcher != null)
+            {
+                watcher.Dispose();
+                watcher = null;
+            }
+        }
+
+        private void SetupFolderListener()
+        {
+            RemoveFolderListener();
+
+            watcher = new FileSystemWatcher(folderPath);
+
+            /*watcher.NotifyFilter = NotifyFilters.Attributes
+                                | NotifyFilters.CreationTime
+                                | NotifyFilters.DirectoryName
+                                | NotifyFilters.FileName
+                                | NotifyFilters.LastAccess
+                                | NotifyFilters.LastWrite
+                                | NotifyFilters.Security
+                                | NotifyFilters.Size;*/
+
+            // Any file changed, created, renamed or deleted in the folder.
+            watcher.Changed += FolderContent_Changed;
+            watcher.Created += FolderContent_Created;
+            watcher.Renamed += FolderContent_Renamed;
+            watcher.Deleted += FolderContent_Deleted;
+
+            // An error happened. Simply re-create the watcher.
+            watcher.Error += FolderContent_Error;
+
+            // It's always a good thing to enable the listener. 
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void FolderContent_Error(object sender, ErrorEventArgs e)
+        {
+            MessageBox.Show("A folder listener exception happened: " + e.GetException().Message, "Restarting listener");
+
+            try
+            {
+                SetupFolderListener();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"Could not recreate the folder listener, no changes you make to the folder will be uploaded. Please restart the app.{Environment.NewLine}Folder: {folderPath}{Environment.NewLine}Exception message: {ex.Message}", "Error");
+            }
+        }
+
+        private void FolderContent_Deleted(object sender, FileSystemEventArgs e)
+        {
+            string fileName = e.Name;
+
+            MessageBox.Show("File deleted: " + fileName);
+
+            // Delete file online as well.
+            fileEvents.FileDeleted(fileName);
+        }
+
+        private void FolderContent_Renamed(object sender, RenamedEventArgs e)
+        {
+            string oldFileName = e.OldName;
+            string newFileName = e.Name;
+
+            MessageBox.Show("File renamed: " + oldFileName + " -> " + newFileName);
+
+            // Rename file online as well.
+            fileEvents.FileRenamed(oldFileName, newFileName);
+        }
+
+        private void FolderContent_Created(object sender, FileSystemEventArgs e)
+        {
+            string fileName = e.Name;
+
+            MessageBox.Show("File created: " + fileName);
+
+            // Upload new file to server.
+            fileEvents.FileCreated(fileName);
+        }
+
+        private void FolderContent_Changed(object sender, FileSystemEventArgs e)
+        {
+            string fileName = e.Name;
+
+            MessageBox.Show("File changed: " + fileName);
+
+            // Upload and overwrite file at server.
+            fileEvents.FileChanged(fileName);
+        }
+
+        public void Dispose()
+        {
+            RemoveFolderListener();
+            fileEvents = null;
         }
     }
 
@@ -46,9 +241,12 @@ namespace Filentropia
     public partial class MainWindow : Window
     {
         private AppState appState = AppState.NoFolderSelected;
-        private string folderPath;
-        private FileSystemWatcher watcher = null;
-        private List<FileEvent> fileEvents = new List<FileEvent>();
+        private string folderOnePath;
+        private string folderTwoPath;
+
+        // These two folders will be synchronized.
+        private FolderListener folderOneListener;
+        private FolderListener folderTwoListener;
 
         // Hmm, tanken är att appen ska tanka upp filerna via ftp till servern, och så ska alla andra
         // som upptäcker förändringarna automatiskt ladda ner nya och förändrade filer. 
@@ -64,7 +262,7 @@ namespace Filentropia
         }
 
         // Allow transitions between states, some of them.
-        public bool SetState(AppState newState, string folderPath = "")
+        public bool SetState(AppState newState, string _folderPath = "")
         {
             switch(appState)
             {
@@ -72,7 +270,7 @@ namespace Filentropia
                     if(newState == AppState.FolderSelected)
                     {
                         // Going from no folder selected to folder selected is ok.
-                        this.folderPath = folderPath;
+                        folderOnePath = _folderPath;
 
                         appState = newState;
                         UpdateInterface();
@@ -83,7 +281,7 @@ namespace Filentropia
                     if (newState == AppState.FolderShared)
                     {
                         // A folder is selected, and now user want to share it. That's ok.
-                        SetupFolderListener();
+                        folderOneListener = new FolderListener(folderOnePath);
 
                         appState = newState;
                         UpdateInterface();
@@ -93,7 +291,7 @@ namespace Filentropia
                     else if(newState == AppState.FolderSelected)
                     {
                         // User wanted to select a new folder, still not shared. That's ok.
-                        this.folderPath = folderPath;
+                        folderOnePath = _folderPath;
 
                         appState = newState;
                         UpdateInterface();
@@ -104,7 +302,8 @@ namespace Filentropia
                     if (newState == AppState.FolderSelected)
                     {
                         // The currently shared folder is unshared, since a new folder is selected. That's ok.
-                        RemoveFolderListener();
+                        folderOneListener.Dispose();
+                        folderOneListener = null;
 
                         appState = newState;
                         UpdateInterface();
@@ -113,8 +312,9 @@ namespace Filentropia
                     else if(newState == AppState.NoFolderSelected)
                     {
                         // The currently shared folder is unshared, since user wanted to unshare it. That's ok.
-                        RemoveFolderListener();
-                        this.folderPath = "none";
+                        folderOneListener.Dispose();
+                        folderOneListener = null;
+                        folderOnePath = null;
 
                         appState = newState;
                         UpdateInterface();
@@ -143,13 +343,13 @@ namespace Filentropia
                     UnshareFolderButton.IsEnabled = false;
                     break;
                 case AppState.FolderSelected:
-                    SelectedFolderLabel.Content = "Selected folder: " + folderPath;
+                    SelectedFolderLabel.Content = "Selected folder: " + folderOnePath;
                     AddFolderButton.Content = "Select another folder";
                     ShareFolderButton.IsEnabled = true;
                     UnshareFolderButton.IsEnabled = false;
                     break;
                 case AppState.FolderShared:
-                    SelectedFolderLabel.Content = "Shared folder: " + folderPath;
+                    SelectedFolderLabel.Content = "Shared folder: " + folderOnePath;
                     AddFolderButton.IsEnabled = false;
                     ShareFolderButton.IsEnabled = false;
                     UnshareFolderButton.IsEnabled = true;
@@ -157,100 +357,6 @@ namespace Filentropia
                 default:
                     throw new Exception("You forgot adding this state! " + appState);
             }
-        }
-
-        private void RemoveFolderListener()
-        {
-            if (watcher != null)
-            {
-                watcher.Dispose();
-                watcher = null;
-            }
-        }
-
-        private bool SetupFolderListener()
-        {
-            try
-            {
-                RemoveFolderListener();
-
-                watcher = new FileSystemWatcher(folderPath);
-
-                /*watcher.NotifyFilter = NotifyFilters.Attributes
-                                 | NotifyFilters.CreationTime
-                                 | NotifyFilters.DirectoryName
-                                 | NotifyFilters.FileName
-                                 | NotifyFilters.LastAccess
-                                 | NotifyFilters.LastWrite
-                                 | NotifyFilters.Security
-                                 | NotifyFilters.Size;*/
-
-                // Any file changed, created, renamed or deleted in the folder.
-                watcher.Changed += FolderContent_Changed;
-                watcher.Created += FolderContent_Created;
-                watcher.Renamed += FolderContent_Renamed;
-                watcher.Deleted += FolderContent_Deleted;
-                
-                // An error happened. Simply re-create the watcher.
-                watcher.Error += FolderContent_Error;
-
-                // It's always a good thing to enable the listener. 
-                watcher.EnableRaisingEvents = true;
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show("Folder listener exception: " + ex.Message);
-                return false;
-            }
-
-            return true;
-        }
-
-        private void FolderContent_Error(object sender, ErrorEventArgs e)
-        {
-            MessageBox.Show("A folder listener exception happened: " + e.GetException().Message, "Restarting listener");
-
-            if (!SetupFolderListener())
-            {
-                MessageBox.Show("Could not recreate the folder listener, no changes you make to the folder will be uploaded. Please restart the app. ", "Error");
-            }
-        }
-
-        private void FolderContent_Deleted(object sender, FileSystemEventArgs e)
-        {
-            string fileName = e.Name;
-
-            MessageBox.Show("File deleted: " + fileName);
-
-            // Delete file online as well.
-        }
-
-        private void FolderContent_Renamed(object sender, RenamedEventArgs e)
-        {
-            string oldFileName = e.OldName;
-            string newFileName = e.Name;
-
-            MessageBox.Show("File renamed: " + oldFileName + " -> " + newFileName);
-
-            // Rename file online as well.
-        }
-
-        private void FolderContent_Created(object sender, FileSystemEventArgs e)
-        {
-            string fileName = e.Name;
-
-            MessageBox.Show("File created: " + fileName);
-
-            // Upload new file to server.
-        }
-
-        private void FolderContent_Changed(object sender, FileSystemEventArgs e)
-        {
-            string fileName = e.Name;
-
-            MessageBox.Show("File changed: " + fileName);
-
-            // Upload and overwrite file at server.
         }
 
         private void AddFolder_Click(object sender, RoutedEventArgs e)
@@ -284,6 +390,17 @@ namespace Filentropia
             else
                 MessageBox.Show("Folder is no longer shared!");
 
+        }
+
+        private void SelectFolder2Button_Click(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                folderTwoPath = dialog.FileName;
+            }
         }
     }
 }
